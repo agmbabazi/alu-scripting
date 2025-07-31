21a5966a27022e6070d98a05b21cddc72a19401c
agmbabazi <a.mbabazi3@alustudent.com>
2025-07-31 12:11:10 -0700

    more commits

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Print top 10 hot post titles for a subreddit"""
 import requests
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'ALU-Reddit-API-Project'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            posts = response.json().get('data', {}).get('children', [])
-            for post in posts:
-                print(post['data'].get('title'))
-            return
-    except Exception:
-        pass
-    print(None)
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'limit': 10}
+    
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+    else:
+        print(None)
--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Recursively get all hot post titles for a subreddit"""
 import requests
 
 def recurse(subreddit, hot_list=[], after=None):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'ALU-Reddit-API-Project'}
-    params = {'after': after}
-    try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        if response.status_code != 200:
-            return None
-        data = response.json().get('data', {})
-        posts = data.get('children', [])
-        for post in posts:
-            hot_list.append(post['data'].get('title'))
-        after = data.get('after')
-        if after:
-            return recurse(subreddit, hot_list, after)
-        return hot_list
-    except Exception:
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'limit': 100}
+    if after:
+        params['after'] = after
+
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    if response.status_code != 200:
         return None
+
+    data = response.json().get('data', {})
+    children = data.get('children', [])
+
+    for child in children:
+        hot_list.append(child['data']['title'])
+
+    after = data.get('after')
+    if after:
+        return recurse(subreddit, hot_list, after)
+    return hot_list
